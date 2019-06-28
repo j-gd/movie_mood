f4041399a98194ddb2ce03aeac47807b2310699c
J-G Demathieu <jdemathieu@gmail.com>
2019-06-27 21:43:34 -0700

    Refactor 7 emotions to pandas dataframe

--- src/emotions_seven.py
+++ src/emotions_seven.py
@@ -1,14 +1,47 @@
-import pickle
+import numpy as np
+import pandas as pd
 
 class Emotions7():
     def __init__(self, test=False):
-        if test:
-            path = 'data/'
+        if test == True:
+            path = '../datasets/'
         else:
-            path = '../data/'
-        pickle_in = open(path + "keywords_seven_emotions.pickle", "rb")
-        self.emotions = pickle.load(pickle_in)
+            path = '../../datasets/'
+        emotions_df = pd.read_csv(path + 'andbrainDataSet108.csv')
+        emotions_df['word'] = emotions_df['word'].apply(lambda w: w.lower().strip())
+        emotions_df.set_index('word',inplace=True)
+        self.emotions_df = emotions_df
+
+    def transform(self, df, column):
+        # Add 7 columns, one for each emotion
+        pass
+
+    def get_emotions(self, word_list,normalize=True):
+        '''
+        INPUT:
+            word_list: list of lowercase words
+
+        OUTPUT
+            panda Series with word_list values for:
+                disgust, surprise, neutral, anger, sad, happy, fear
+        '''
+        word_emotions = pd.Series(
+            np.zeros((len(self.emotions_df.columns))), index=self.emotions_df.columns)
+
+        for word in word_list:
+            if word in self.emotions_df.index:
+                word_emotions += self.emotions_df.loc[word,:]
+
+        if normalize:
+            # normalize the vector: best reviews have very small text compared to others
+            word_emotions = word_emotions / np.sqrt(
+                np.dot(word_emotions, word_emotions))
+
+        return word_emotions
 
 if __name__ == "__main__":
     e = Emotions7()
-
+    words = ['abuse','aaa']
+    print('Emotions for:', words)
+    print(e.get_emotions(words, normalize=False))
+    print(e.get_emotions(words))
--- test/unittests.py
+++ test/unittests.py
@@ -8,7 +8,6 @@ import numpy as np
 from src.emotions_seven import Emotions7
 
 
-
 class TestEmotions7(unittest.TestCase):
     def setUp(self):
         self.emotions = Emotions7(test=True)
@@ -17,8 +16,11 @@ class TestEmotions7(unittest.TestCase):
         self.emotions = None
 
     def test_class_emotions(self):
-        self.assertEqual(len(self.emotions.emotions.keys()), 23729)
-
+        self.assertEqual(list(self.emotions.emotions_df.columns),
+            ['disgust', 'surprise', 'neutral', 'anger', 'sad', 'happy', 'fear'])
+        self.assertEqual(
+            self.emotions.get_emotions(
+                ['abuse','aaa'], normalize=False).loc['anger'], 0.13736264)
 
 if __name__ == '__main__':
     unittest.main()
