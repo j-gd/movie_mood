7119974e024b93cc0469c7fdc2e1e175160b9d88
J-G Demathieu <jdemathieu@gmail.com>
2019-07-19 13:26:08 -0700

    Initial SubjectiveFilter py code

---
+++ src/subjective_filter.py
@@ -0,0 +1,13 @@
+class SubjectiveFilter():
+  def __init__(self):
+    '''
+    Input:
+      A Pandas dataframe
+      text column name
+
+    Output:
+      A pandas dataframe with objective sentences removed 
+    '''
+    pass
+
+    # save 
